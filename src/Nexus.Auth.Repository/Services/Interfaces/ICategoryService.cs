e935e2205c19a3122e50af3261627ab7dec2373b
Thaian Carvalho <thaian.carvalho@luby.com.br>
2023-09-20 12:22:40 -0300

    feat: add category endpoint

---
+++ src/Nexus.Auth.Repository/Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,8 @@
+﻿using Nexus.Auth.Repository.Dtos;
+using Nexus.Auth.Repository.Interfaces;
+
+namespace Nexus.Auth.Repository.Services.Interfaces;
+
+public interface ICategoryService : IGenericService<CategoryDto, CategoryResponseDto>
+{
+}
