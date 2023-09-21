f86f7fde9348fc65ef4cfbebd96ae42ec163f526
Thaian Carvalho <thaian.carvalho@luby.com.br>
2023-09-21 12:04:04 -0300

    feat: add delay reason endpoint

---
+++ src/Nexus.Auth.Api/Controllers/DelayReasonController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Nexus.Auth.Repository.Dtos.Generics;
+using Nexus.Auth.Repository.Dtos;
+using Nexus.Auth.Repository.Services.Interfaces;
+
+namespace Nexus.Auth.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class DelayReasonController : ControllerBase
+    {
+        private readonly IDelayReasonService _delayReasonService;
+        private readonly IConfiguration _configuration;
+
+        public DelayReasonController(IDelayReasonService delayReasonService, IConfiguration configuration)
+        {
+            _delayReasonService = delayReasonService;
+            _configuration = configuration;
+        }
+
+        /// GET: api/v1/DelayReason/GetAll
+        /// <summary>
+        /// Endpoint to get all delayReasons
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("GetAll")]
+        public async Task<IActionResult> GetAll(PageParams pageParams) => Ok(await _delayReasonService.GetAll(pageParams, _configuration["ConnectionStrings:NexusVpcApi"]));
+
+        /// POST: api/v1/DelayReason/GetById
+        /// <summary>
+        /// Endpoint to get delayReason by id
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("GetById")]
+        public async Task<IActionResult> GetById(GetById obj) => Ok(await _delayReasonService.GetById(obj, _configuration["ConnectionStrings:NexusVpcApi"]));
+
+        /// POST: api/v1/DelayReason/GetByName
+        /// <summary>
+        /// Endpoint to get delayReasons by name
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("GetByName")]
+        public async Task<IActionResult> GetByName(GetByName obj) => Ok(await _delayReasonService.GetByName(obj, _configuration["ConnectionStrings:NexusVpcApi"]));
+
+        /// POST: api/v1/DelayReason/Post
+        /// <summary>
+        /// Endpoint to create new delayReason
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("Post")]
+        public async Task<IActionResult> Post(DelayReasonDto obj)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState.Root.Errors);
+
+            var response = await _delayReasonService.Post(obj, _configuration["ConnectionStrings:NexusVpcApi"]);
+            return response.Success ? Ok(response) : BadRequest(response);
+        }
+
+        /// POST: api/v1/DelayReason/Put
+        /// <summary>
+        /// Endpoint to change an delayReason
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("Put")]
+        public async Task<IActionResult> Put(DelayReasonPutDto obj)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState.Root.Errors);
+
+            var response = await _delayReasonService.Put(obj, _configuration["ConnectionStrings:NexusVpcApi"]);
+            return response.Success ? Ok(response) : BadRequest(response);
+        }
+
+        /// POST: api/v1/DelayReason/Remove
+        /// <summary>
+        /// Endpoint to remove an delayReason
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("Remove")]
+        public async Task<IActionResult> Remove(GetById obj)
+        {
+            var response = await _delayReasonService.Delete(obj, _configuration["ConnectionStrings:NexusVpcApi"]);
+            return response.Success ? Ok(response) : NotFound(response);
+        }
+    }
+}
---
+++ src/Nexus.Auth.Repository/Services/DelayReasonService.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Nexus.Auth.Repository.Dtos.Generics;
+using Nexus.Auth.Repository.Dtos;
+using Nexus.Auth.Repository.Services.Interfaces;
+using Nexus.Auth.Repository.Utils;
+
+namespace Nexus.Auth.Repository.Services;
+
+public class DelayReasonService : IDelayReasonService
+{
+
+    private readonly IAccessDataService _accessDataService;
+
+    public DelayReasonService(IAccessDataService accessDataService)
+    {
+        _accessDataService = accessDataService;
+    }
+
+    public async Task<GenericCommandResult<IEnumerable<DelayReasonResponseDto>>> GetAll(PageParams pageParams, string path)
+    {
+        var result = await _accessDataService.PostDataAsync<IEnumerable<DelayReasonResponseDto>>(path, "api/v1/DelayReason/GetAll", pageParams);
+        if (result is not null)
+            return new GenericCommandResult<IEnumerable<DelayReasonResponseDto>>(true, "Success", result, StatusCodes.Status200OK);
+
+        return new GenericCommandResult<IEnumerable<DelayReasonResponseDto>>(true, "Error", default, StatusCodes.Status400BadRequest);
+    }
+
+    public async Task<GenericCommandResult<DelayReasonResponseDto>> GetById(GetById obj, string path)
+    {
+        var result = await _accessDataService.PostDataAsync<DelayReasonResponseDto>(path, "api/v1/DelayReason/GetById", obj);
+        if (result is not null)
+            return new GenericCommandResult<DelayReasonResponseDto>(true, "Success", result, StatusCodes.Status200OK);
+
+        return new GenericCommandResult<DelayReasonResponseDto>(true, "Error", default, StatusCodes.Status400BadRequest);
+    }
+
+    public async Task<GenericCommandResult<IEnumerable<DelayReasonResponseDto>>> GetByName(GetByName obj, string path)
+    {
+        var result = await _accessDataService.PostDataAsync<IEnumerable<DelayReasonResponseDto>>(path, "api/v1/DelayReason/GetByName", obj);
+        if (result is not null)
+            return new GenericCommandResult<IEnumerable<DelayReasonResponseDto>>(true, "Success", result, StatusCodes.Status200OK);
+
+        return new GenericCommandResult<IEnumerable<DelayReasonResponseDto>>(true, "Error", default, StatusCodes.Status400BadRequest);
+    }
+
+    public async Task<GenericCommandResult<DelayReasonResponseDto>> Post(DelayReasonDto obj, string path)
+    {
+        var result = await _accessDataService.PostDataAsync<DelayReasonResponseDto>(path, "api/v1/DelayReason/Post", obj);
+        if (result is not null)
+            return new GenericCommandResult<DelayReasonResponseDto>(true, "Success", result, StatusCodes.Status200OK);
+
+        return new GenericCommandResult<DelayReasonResponseDto>(true, "Error", default, StatusCodes.Status400BadRequest);
+    }
+
+    public async Task<GenericCommandResult<DelayReasonResponseDto>> Put(DelayReasonDto obj, string path)
+    {
+        var result = await _accessDataService.PostDataAsync<DelayReasonResponseDto>(path, "api/v1/DelayReason/Put", obj);
+        if (result is not null)
+            return new GenericCommandResult<DelayReasonResponseDto>(true, "Success", result, StatusCodes.Status200OK);
+
+        return new GenericCommandResult<DelayReasonResponseDto>(true, "Error", default, StatusCodes.Status400BadRequest);
+    }
+
+    public async Task<GenericCommandResult<TokenDto>> Delete(GetById obj, string path)
+    {
+        var result = await _accessDataService.PostDataAsync<TokenDto>(path, "api/v1/DelayReason/Delete", obj);
+        if (result is not null)
+            return new GenericCommandResult<TokenDto>(true, "Success", result, StatusCodes.Status200OK);
+
+        return new GenericCommandResult<TokenDto>(true, "Error", default, StatusCodes.Status400BadRequest);
+    }
+}
