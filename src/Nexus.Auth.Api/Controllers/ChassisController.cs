b1821bbe06722b9ae47cc867d1dcdbb29bfa8032
Diego Francisco <diego.francisco@luby.software>
2023-12-01 15:31:55 -0300

    Create endpoint to load chassi by serial number

--- src/Nexus.Auth.Api/Controllers/ChassisController.cs
+++ src/Nexus.Auth.Api/Controllers/ChassisController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Nexus.Auth.Repository.Dtos.Chassis;
 using Nexus.Auth.Repository.Dtos.Generics;
 using Nexus.Auth.Repository.Services.Interfaces;
 
@@ -35,5 +36,13 @@ namespace Nexus.Auth.Api.Controllers
         /// <returns></returns>
         [HttpPost("GetById")]
         public async Task<IActionResult> GetById(GetById obj) => Ok(await _chassisService.GetById(obj, _configuration["ConnectionStrings:NexusVehicleApi"]));
+
+        /// POST: api/v1/Chassis/GetBySerialNumber
+        /// <summary>
+        /// Endpoint to get chassis by serial number
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("GetBySerialNumber")]
+        public async Task<IActionResult> GetBySerialNumber(GetBySerialNumber obj) => Ok(await _chassisService.GetBySerialNumber(obj, _configuration["ConnectionStrings:NexusVehicleApi"]));
     }
 }
---
+++ src/Nexus.Auth.Repository/Dtos/Chassis/GetBySerialNumber.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Nexus.Auth.Repository.Dtos.Chassis
+{
+    public class GetBySerialNumber
+    {
+        [MaxLength(6)]
+        [MinLength(6)]
+        public required string SerialNumber { get; set; }
+    }
+}
