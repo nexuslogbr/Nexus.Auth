bfba0ac3c774110b012f48231e1ea87bb6559acc
Diego Francisco <diego.francisco@luby.software>
2024-01-17 15:25:31 -0300

    Criação de serviço para consultar solicitante por codigo

---
+++ src/Nexus.Auth.Repository/Dtos/Generics/GetByNameCodeDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Nexus.Auth.Repository.Dtos.Generics
+{
+    public class GetByNameCodeDto
+    {
+        public string? Name { get; set; }
+        public string? Code { get; set; }
+    }
+}
